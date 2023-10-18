c50333adaa268468f72cae5883ccf3bb20d9b8f2
Swapnil Bhabal <swapnil.bhabal@tweeny.in>
2023-10-18 17:42:51 +0530

    Testing node modules again

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port number ${PORT}`
   );
 });
