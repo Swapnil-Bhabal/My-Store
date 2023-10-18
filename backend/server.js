752f944f5adb680f4105b49208bca3d2bebf7851
Swapnil Bhabal <swapnil.bhabal@tweeny.in>
2023-10-18 18:45:27 +0530

    Step 12, 13 and 14 completed. Install and setup MongoDB and Connect to the database done and done with installation of colors as well.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
+import colors from "colors";
 import dotenv from "dotenv";
 import express from "express";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -23,6 +27,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port number ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port number ${PORT}`.rainbow
   );
 });
